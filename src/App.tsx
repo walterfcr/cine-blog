b05189288ceb4427fae35b811c753b65d27ca165
walterfcr <143011753+walterfcr@users.noreply.github.com>
2026-06-27 19:12:30 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-950">
+      <h1 className="text-5x1 font-bold text-white">Cine Blog</h1>
+    </main>
+  )
+}
+
+export default App
