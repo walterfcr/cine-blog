b782158170cc019842e485845c0c25499cd24fd3
walterfcr <143011753+walterfcr@users.noreply.github.com>
2026-06-27 21:17:06 -0600

    configured alias

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import MainLayout from '../layouts/MainLayout'
-
-import Home from '../pages/Home'
-import Movies from '../pages/Movies'
-import Reviews from '../pages/Reviews'
-import About from '../pages/About'
-import NotFound from '../pages/NotFound'
+import MainLayout from '@/layouts/MainLayout'
+import Home from '@/pages/Home'
+import Movies from '@/pages/Movies'
+import Reviews from '@/pages/Reviews'
+import About from '@/pages/About'
+import NotFound from '@/pages/NotFound'
 
 export const router = createBrowserRouter([
   {
