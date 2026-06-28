11038e491eb92f2a0ec23b29d198fe14010feff7
walterfcr <143011753+walterfcr@users.noreply.github.com>
2026-06-27 19:26:03 -0600

    testing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-slate-950">
-      <h1 className="text-5x1 font-bold text-white">Cine Blog</h1>
+      <h1 className="text-5xl font-bold text-white">Cine Blog</h1>
     </main>
   )
 }
